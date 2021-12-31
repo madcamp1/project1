2d8a2b52507bb43da136335e99334549a828e0f7
ibasaw54 <awwsb41@gmail.com>
2021-12-31 11:10:55 +0900

    tq

--- app/src/main/java/com/example/firstapp/Map.java
+++ app/src/main/java/com/example/firstapp/Map.java
@@ -1,9 +1,16 @@
 package com.example.firstapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,14 +26,23 @@ public class Map extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        int a = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        int b = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION);
+        int c = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.INTERNET);
+        Log.d("PERMISSION", Integer.toString(a));
+        Log.d("PERMISSION", Integer.toString(b));
+        Log.d("PERMISSION", Integer.toString(c));
         rootView = (ViewGroup) inflater.inflate(R.layout.map_fragment, container, false);
         MapView mapView = new MapView(requireContext());
-        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(33.41, 126.52), true);
-        mapView.setZoomLevel(10, true);
-        mapView.setMapViewEventListener((MapView.MapViewEventListener) requireContext());
-        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
+//        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(33.41, 126.52), true);
+//        mapView.setZoomLevel(10, true);
+//        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
         ViewGroup mapViewGroup = rootView.findViewById(R.id.map_view);
         mapViewGroup.addView(mapView);
         return rootView;
+    }
+
+    public boolean locationServiceStatus(){
+        return false;
     }
 }
